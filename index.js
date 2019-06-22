76352dc89b12f9daf2549088c5b7cd458b3c4521
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2019-06-22 13:41:03 -0600

    testing deployment

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
+    res.send({ 
+        hi: 'there',
+        bye: 'buddy'
+    })
 })
 
 app.listen(PORT, (err) => err ? console.warn(err): console.log(`Listening on port: ${PORT}`))
